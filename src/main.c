35f9450ad9f1bd4c3aab013a5d540d89060ce032
kakaomame <nattyannkunndesu@icloud.com>
2026-06-25 21:37:36 +0900

    main.c の更新

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-#include <emscripten.h>
+#include <dlfcn.h>
 
 int main() {
-    printf("Gemini Programming隊: Android-like Environment Build SUCCESS!\n");
+    // コンパイル時に指定したライブラリをロード
+    void *handle = dlopen("library.wasm", RTLD_NOW);
+    if (!handle) {
+        printf("Error: Could not load library\n");
+        return 1;
+    }
+
+    // 関数ポインタを取得
+    int (*add_func)(int, int) = dlsym(handle, "add");
+    
+    // 実行
+    int result = add_func(10, 20);
+    printf("Result from dynamic library: %d\n", result);
+
+    dlclose(handle);
     return 0;
 }
