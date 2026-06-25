52f51b95d565e06bceb3796635d1a7ff6ef945bc
kakaomame <nattyannkunndesu@icloud.com>
2026-06-25 22:17:52 +0900

    loder.h の作成

---
+++ src/loder.h
@@ -0,0 +1,11 @@
+#ifndef LOADER_H
+#define LOADER_H
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+// ヘッダーファイル：これを読み込めばloaderの機能が使える
+int adaptive_open(const char *pathname, int flags, mode_t mode);
+int ensure_dir_exists(const char *path);
+
+#endif
