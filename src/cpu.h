444eb4cf6427d3b3fc38e72c9cfcc747c4858fc4
kakaomame <nattyannkunndesu@icloud.com>
2026-06-25 23:19:51 +0900

    cpu.h の更新

--- src/cpu.h
+++ src/cpu.h
@@ -1,12 +1,20 @@
 #ifndef CPU_H
 #define CPU_H
+
 #include <stdint.h>
 
+// ARM64のレジスタを模倣する
 typedef struct {
-    uint64_t x[31]; // 汎用レジスタ
+    uint64_t x[31]; // X0-X30
     uint64_t pc;    // プログラムカウンタ
     uint64_t sp;    // スタックポインタ
+    uint32_t nzcv;  // フラグ
 } CPUState;
 
-void cpu_execute_step(CPUState *cpu); // 命令を1つずつ解釈する
+// CPUの状態を初期化する
+void cpu_init(CPUState *cpu);
+
+// 1命令だけ実行する（ここが命令解釈のメインになる）
+void cpu_execute_step(CPUState *cpu);
+
 #endif
