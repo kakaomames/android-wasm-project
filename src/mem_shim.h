2288796b19927b2117d303dcc08d8b1a41cc6b97
kakaomame <nattyannkunndesu@icloud.com>
2026-06-26 01:18:59 +0900

    mem_shim.h の更新

--- src/mem_shim.h
+++ src/mem_shim.h
@@ -1,5 +1,8 @@
 #ifndef MEM_SHIM_H
 #define MEM_SHIM_H
+#ifndef MAP_FAILED
+#define MAP_FAILED ((void *)-1)
+#endif
 
 #include <stddef.h>
 #include <sys/types.h>
@@ -8,4 +11,7 @@
 void init_guest_memory(size_t total_size);
 void* guest_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 
-#endif // MEM_SHIM_H
+void init_guesttt_memory(size_t total_size);
+void* guest_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+
+#endif
