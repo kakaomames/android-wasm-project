23a0c9115550d9df01ef4327e3e966fc048a1060
kakaomame <nattyannkunndesu@icloud.com>
2026-06-26 01:23:29 +0900

    loader.h の更新

--- src/loader.h
+++ src/loader.h
@@ -7,5 +7,6 @@
 // ヘッダーファイル：これを読み込めばloaderの機能が使える
 int adaptive_open(const char *pathname, int flags, mode_t mode);
 int ensure_dir_exists(const char *path);
+void load_library(const char* lib_name);
 
 #endif
