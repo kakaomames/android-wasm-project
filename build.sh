f2035fe1c19814f2e900dca532886119c8db9523
kakaomame <nattyannkunndesu@icloud.com>
2026-06-26 14:59:28 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -69,6 +69,7 @@ echo "emcc start!"
 # src/gpu.c を忘れずに追加した完全版コマンド
 # 全ユニットを統合するコマンド
 EMCC_DEBUG=1 emcc src/main.c stubs.c src/loader.c src/mem_shim.c src/cpu.c src/gpu.c src/gps.c src/media.c \
+    --preload-file build_out@/libs \
     -o build_out/1.html \
     -s WASM=1 \
     -s "EXPORTED_RUNTIME_METHODS=['ccall','cwrap','HEAPU8']" \
