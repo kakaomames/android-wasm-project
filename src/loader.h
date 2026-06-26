5e272b5d6e92a13c2f864edbc13e0a677828730f
kakaomame <nattyannkunndesu@icloud.com>
2026-06-26 15:52:14 +0900

    Update loader.h

--- src/loader.h
+++ src/loader.h
@@ -3,10 +3,20 @@
 
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <stdint.h> // uint8_t のために必要
 
-// ヘッダーファイル：これを読み込めばloaderの機能が使える
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int adaptive_open(const char *pathname, int flags, mode_t mode);
 int ensure_dir_exists(const char *path);
 void load_library(const char* lib_name);
+// 追記：これがないと main.c から呼べない
+void load_elf_to_guest(uint8_t *file_data, const char *name);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
